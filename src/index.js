52fcd4d7519d096c651b55f8be2c2a1f5ec89c33
guidorauber <guidomrauber@gmail.com>
2020-07-13 16:33:39 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const app = express();
 
+// No CORS Headder set
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/message.json');
+});
+
+// CORS header `Access-Control-Allow-Origin` set to accept all
+app.get('/allow-cors', function(request, response) {
+  response.set('Access-Control-Allow-Origin', '*');
+  response.sendFile(__dirname + '/message.json');
+});
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // Settings
 app.set('port', process.env.PORT || 3000);
 
